1a24baae13f4baa489859c33b677c0db68a217d7
Mike Patterson <niiimon@gmail.com>
2021-01-28 21:34:36 +0100

    hello

--- dist/index.modern.js
+++ dist/index.modern.js
@@ -1,84 +1,101 @@
 import React__default, { createElement } from 'react';
 import styled, { css } from 'styled-components';
 
-const applyModifier = (modifier, css) => ({
-  modifiers
-}) => {
-  if (!modifiers) return null;
-  return modifiers === modifier || modifiers.includes(modifier) ? css : null;
+function _objectWithoutPropertiesLoose(source, excluded) {
+  if (source == null) return {};
+  var target = {};
+  var sourceKeys = Object.keys(source);
+  var key, i;
+
+  for (i = 0; i < sourceKeys.length; i++) {
+    key = sourceKeys[i];
+    if (excluded.indexOf(key) >= 0) continue;
+    target[key] = source[key];
+  }
+
+  return target;
+}
+
+function _taggedTemplateLiteralLoose(strings, raw) {
+  if (!raw) {
+    raw = strings.slice(0);
+  }
+
+  strings.raw = raw;
+  return strings;
+}
+
+var applyModifier = function applyModifier(modifier, css) {
+  return function (_ref) {
+    var modifiers = _ref.modifiers;
+    if (!modifiers) return null;
+    return modifiers === modifier || modifiers.includes(modifier) ? css : null;
+  };
 };
 
-let _ = t => t,
-    _t,
-    _t2,
-    _t3;
+function _templateObject3() {
+  var data = _taggedTemplateLiteralLoose(["\n        /* background: orange; */\n      "]);
 
-const Button = ({
-  children,
-  className,
-  ...props
-}) => {
+  _templateObject3 = function _templateObject3() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject2() {
+  var data = _taggedTemplateLiteralLoose(["\n        padding: 0;\n      "]);
+
+  _templateObject2 = function _templateObject2() {
+    return data;
+  };
+
+  return data;
+}
+
+function _templateObject() {
+  var data = _taggedTemplateLiteralLoose(["\n    appearance: none;\n    background: none;\n    display: inline-block;\n    border: 2px solid transparent;\n    background-color: ", ";\n    color: ", ";\n    font-size: 2rem;\n    padding: 20px;\n    transition: 0.15s ease background-color, color;\n    cursor: pointer;\n    ", ";\n    ", "\n\n    &:hover {\n      background-color: white;\n      color: black;\n      border-color: black;\n    }\n\n    ", " {\n      background: orange;\n    }\n    ", " {\n      background: red;\n    }\n    ", " {\n      background: purple;\n    }\n    ", " {\n      background: ", ";\n    }\n\n    ", "\n\n    ", "\n  "]);
+
+  _templateObject = function _templateObject() {
+    return data;
+  };
+
+  return data;
+}
+
+var Button = function Button(_ref) {
+  var children = _ref.children,
+      className = _ref.className,
+      props = _objectWithoutPropertiesLoose(_ref, ["children", "className"]);
+
   return React__default.createElement("button", Object.assign({
     className: className,
-    onMouseDown: e => e.preventDefault()
+    onMouseDown: function onMouseDown(e) {
+      return e.preventDefault();
+    }
   }, props), children);
 };
 
-var Button$1 = styled(Button)(({
-  theme: t
-}) => css(_t || (_t = _`
-    appearance: none;
-    background: none;
-    display: inline-block;
-    border: 2px solid transparent;
-    background-color: ${0};
-    color: ${0};
-    font-size: 2rem;
-    padding: 20px;
-    transition: 0.15s ease background-color, color;
-    cursor: pointer;
-    ${0};
-    ${0}
+var Button$1 = styled(Button)(function (_ref2) {
+  var t = _ref2.theme;
+  return css(_templateObject(), t.colors.text, t.colors.background, t.fonts.h1(), t.spacing.section('mt'), t.bp.sm, t.bp.md, t.bp.lg, t.bp.xl, t.color.darken('red', 0.25), applyModifier('small', css(_templateObject2())), applyModifier('active', css(_templateObject3())));
+});
 
-    &:hover {
-      background-color: white;
-      color: black;
-      border-color: black;
-    }
+function _templateObject$1() {
+  var data = _taggedTemplateLiteralLoose(["\n    ", ";\n    background: ", ";\n  "]);
 
-    ${0} {
-      background: orange;
-    }
-    ${0} {
-      background: red;
-    }
-    ${0} {
-      background: purple;
-    }
-    ${0} {
-      background: ${0};
-    }
+  _templateObject$1 = function _templateObject() {
+    return data;
+  };
 
-    ${0}
-
-    ${0}
-  `), t.colors.text, t.colors.background, t.fonts.h1(), t.spacing.section('mt'), t.bp.sm, t.bp.md, t.bp.lg, t.bp.xl, t.color.darken('red', 0.25), applyModifier('small', css(_t2 || (_t2 = _`
-        padding: 0;
-      `))), applyModifier('active', css(_t3 || (_t3 = _`
-        /* background: orange; */
-      `)))));
-
-let _$1 = t => t,
-    _t$1;
-const Wrapper = styled.div(({
-  theme
-}) => css(_t$1 || (_t$1 = _$1`
-    ${0};
-    background: ${0};
-  `), theme.spacing.md('my'), theme.colors.background));
-const ExampleComponent = ({
-  text
-}) => {
+  return data;
+}
+var Wrapper = styled.div(function (_ref) {
+  var theme = _ref.theme;
+  return css(_templateObject$1(), theme.spacing.md('my'), theme.colors.background);
+});
+var ExampleComponent = function ExampleComponent(_ref2) {
+  var text = _ref2.text;
   return createElement(Wrapper, null, "Example Component hello: ", text);
 };
 
